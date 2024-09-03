f7fa326505a0f93b3c941c16f0a74edad9676a83
Leonard wegmann simon <leonard.wegmannsimon@elev.ga.ntig.se>
2024-09-03 10:16:35 +0200

    start

---
+++ BashMonster/enemy.cs
@@ -0,0 +1,8 @@
+public class enemy
+{
+    public int hp = 100;
+    public int dmg = 25;
+    public string name = "Monster";
+    
+    
+}
---
+++ BashMonster/player.cs
@@ -0,0 +1,6 @@
+public class player
+{
+public int hp = 100;
+public int dmg = 15;
+public string name = "Simon";
+}
